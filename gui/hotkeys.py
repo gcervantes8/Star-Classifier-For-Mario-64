f4dfe9a07d9259abb5ad690cb742a0503e8291be
gerardocervantes8 <gcervantes8@miners.utep.edu>
2018-08-17 20:15:47 -0600

    Added hotkeys file

---
+++ gui/hotkeys.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 12 16:13:21 2018
+
+@author: Jerry C
+"""
+
+class Hotkeys():
+    
+    split_key = '{PGUP}'
+    reset_key = ''
+     
+    def __init__(self):
+        pass
+    
+    def set_split_key(self, split_key):
+        self.split_key = split_key
+        
+    def set_reset_key(self, reset_key):
+        self.reset_key = reset_key
+        
+    def get_hotkeys(self):
+        return self.split_key, self.reset_key
+    
+    def get_split_key(self):
+        return self.split_key
+    
+    def get_reset_key(self):
+        return self.reset_key
