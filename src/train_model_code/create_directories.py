6abc380b1ee88733502b8adb36a331ecf44ce8d9
gerardocervantes8 <gcervantes8@miners.utep.edu>
2018-10-25 22:12:09 -0600

    Implemented moving images from subfolders into other subfolders to make it easier to setup for training a model

--- src/train_model_code/create_directories.py
+++ src/train_model_code/create_directories.py
@@ -5,7 +5,7 @@ Created on Mon Jan  1 15:36:46 2018
 @author: Gerardo Cervantes
 """
 import os
-
+import shutil
 
 #path is the place where the main directory structure will be made
 #star_amount should be 120 if doing 120 stars
@@ -37,11 +37,34 @@ def create_subdirectory(directory_path, dir_name):
         if not os.path.exists(create_directory):
             os.makedirs(create_directory)
             
-if __name__ == "__main__":
+#Moves images from a specific player from 1 main directory to another directory
+def move_directories(main_src_dir, main_dst_dir, player_dir_name):
     
+    for directory in os.walk(main_src_dir):
+        
+        _, folder_name = os.path.split(directory[0])
+        
+        try:
+            int(folder_name)
+        except ValueError:
+            print('Directory structure did not match, folders have incorrect names')
+            continue
+        
+        
+        src_path = main_src_dir + '/' + folder_name + '/' + player_dir_name
+        dst_path = main_dst_dir + '/' + folder_name + '/' + player_dir_name
+        try:
+            files = os.listdir(src_path)
+        except FileNotFoundError:
+            continue
+        for f in files:
+            shutil.move(src_path + '/' + f, dst_path)
+            
+if __name__ == "__main__":
+    pass
     #This will prepare the directory for training by adding directories
-    path_to_prepare = r'E:\MarioStarClassifier\test_images'
-    stars = 120
-    create_main_directory(path_to_prepare, stars)
-    create_subdirectory(path_to_prepare, 'ZDez_120_1-46-19')
+#    path_to_prepare = r'E:\MarioStarClassifier\test_images'
+#    stars = 120
+#    create_main_directory(path_to_prepare, stars)
+#    create_subdirectory(path_to_prepare, 'ZDez_120_1-46-19')
 
