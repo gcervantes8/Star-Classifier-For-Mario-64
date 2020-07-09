5bbf4bc05f7fb42fa67e70654cebb589dde44c2a
gerardocervantes8 <gcervantes8@miners.utep.edu>
2020-07-09 17:15:59 -0600

    Fixed bugs in the BUILD file! And changed library versions.  Icon also now included

--- BUILD.bat
+++ BUILD.bat
@@ -1,29 +1,32 @@
 REM Running this will build the star classifier from the code
-REM Pre-requisite, you will need Python and Pip command set in the PATH variable
+REM Pre-requisite, you will need Python 3.5 to 3.7 and Pip command set in the PATH variable
 
 SET directory_name=Star-Classifier-v0.96
 
 mkdir python-build-venv
 python -m venv ./python-build-venv
 CALL .\python-build-venv\Scripts\activate
+pip install wheel
 pip install pillow
 pip install pyautogui
 pip install mss
 pip install pypiwin32
 pip install pynput
-pip install tensorflow
-pip install keras
-pip install pyinstaller
-pyinstaller __init__.py --windowed
+pip install tensorflow==1.14
+pip install keras==2.2.5
+pip install https://github.com/pyinstaller/pyinstaller/archive/develop.zip
+pyinstaller  --windowed --icon=images\icon-ico.ico __init__.py
+del __init__.spec
 rmdir /Q /S build
-move dist/__init__ %foo%
+rmdir /Q /S %directory_name% REM delete any previously created builds with same name
+move dist/__init__ %directory_name%
 rmdir /Q /S dist
-mkdir %foo%\images
-mkdir %foo%\routes
-mkdir %foo%\models
-Xcopy /E /I images %foo%\images
-Xcopy /E /I routes %foo%\routes
-Xcopy /E /I models %foo%\models
-move %foo%\__init__.exe %foo%\Star-Classifier.exe
+mkdir %directory_name%\images
+mkdir %directory_name%\routes
+mkdir %directory_name%\models
+Xcopy /E /I images %directory_name%\images
+Xcopy /E /I routes %directory_name%\routes
+Xcopy /E /I models %directory_name%\models
+move %directory_name%\__init__.exe %directory_name%\Star-Classifier.exe
 
 rmdir /Q /S python-build-venv
