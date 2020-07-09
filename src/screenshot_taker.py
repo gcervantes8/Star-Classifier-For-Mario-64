7583b92134577b14393140f35c67abe91e2df7b0
gerardocervantes8 <gcervantes8@miners.utep.edu>
2020-07-08 22:31:47 -0600

    Cleaned up taking screenshots, removed unused code

--- src/screenshot_taker.py
+++ src/screenshot_taker.py
@@ -5,59 +5,29 @@
 """
 from PIL import Image, ImageGrab
 
-#win32 interface is the fastest, mss is next, and pil is the slowest
-
 from mss import mss
 import time
 
-class ScreenshotTaker():
+# win32 library is the fastest (need to select window), mss is next, and pil is the slowest
+
+
+class ScreenshotTaker:
     
-    #sct is mss instance, should be created, only need to be created once and can be reused. Used for mss screenshotting
+    # sct is mss screenshot instance, only need to be created once and can be reused
     sct = None
     
-    #Screenshots using mss library. (faster than screnshotting using PIL if same mss instance is used)
-    
-    #Top and left are coordinates of the top-left of the window where you want the screenshot
-    #Returns 2-tuple of the Pil image and time it took to make screenshot: (img, screenshot_time)
+    # Top and left are coordinates of the top-left of the window where you want the screenshot
+    # Returns 2-tuple of the Pil image and time it took to make screenshot: (img, screenshot_time)
     def screenshot_mss(self, x, y, width, height):
         start_time = time.time()
-        if self.sct == None:
-            self.sct = mss()
-        
-        # The screen part to capture
+        if not ScreenshotTaker.sct:
+            ScreenshotTaker.sct = mss()
+
         monitor = {'top': y, 'left': x, 'width': width, 'height': height}
-    
-        # Grab the data
-        sct_img = self.sct.grab(monitor)
+
+        sct_img = ScreenshotTaker.grab(monitor)
     
         # Create the Image, converts to PIL, probably not needed for speed, but simplifies crop and resize
         img = Image.frombytes('RGB', sct_img.size, sct_img.rgb)
         screenshot_time = time.time() - start_time
         return img, screenshot_time
-    
-    #Screenshots using PIL library
-    def screenshot_pil(self, x, y, width, height):
-        full_img = ImageGrab.grab(bbox=(x, y, x+width, y+height))
-        return full_img
-
-
-    #Has problem finding Amarec program, finds the windows with 'AmaRecTV', but a black screenshot
-    #Is supposed to be able to create fast screenshots
-#    def fast_screenshot(self, window_name, save_name, w, h):
-#        import win32gui
-#        import win32ui
-#        import win32con
-#        hwnd = win32gui.FindWindow(None, window_name)
-#        wDC = win32gui.GetWindowDC(hwnd)
-#        dcObj=win32ui.CreateDCFromHandle(wDC)
-#        cDC=dcObj.CreateCompatibleDC()
-#        dataBitMap = win32ui.CreateBitmap()
-#        dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
-#        cDC.SelectObject(dataBitMap)
-#        cDC.BitBlt((0,0),(w, h) , dcObj, (0,0), win32con.SRCCOPY)
-#        dataBitMap.SaveBitmapFile(cDC, save_name)
-#        #Free ResourcesS
-#        dcObj.DeleteDC()
-#        cDC.DeleteDC()
-#        win32gui.ReleaseDC(hwnd, wDC)
-#        win32gui.DeleteObject(dataBitMap.GetHandle())
